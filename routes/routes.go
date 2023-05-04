d282a8bdc78cb0e865043257a25a2ea5a5dc4ed2
RezaHayati <114706894+rezhnncoding@users.noreply.github.com>
2023-05-05 01:13:35 +0430

    Initial commit

---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"goreact_auth/controllers"
+)
+
+func Setup(app *fiber.App) {
+
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Get("/api/user", controllers.User)
+	app.Post("/api/logout", controllers.Logout)
+
+}
